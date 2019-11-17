832d6daf4cf6791583b044f88d32dac903adcd26
JayJayDee <jindongp@gmail.com>
2019-11-17 15:00:05 +0900

    removed unusing codes.

--- src/playground.ts
+++ src/playground.ts
@@ -1,20 +1,9 @@
-import { launch } from 'puppeteer';
-
 import { initLogger } from './logger';
 import { initConfigurations } from './configurations';
-import { pickSite, initSites } from './site-commands';
-import sellArticles from './site-commands/site-clien/sell-articles';
+import { initSites } from './site-commands';
 
 (async () => {
   initLogger();
   initConfigurations();
   initSites();
-
-  const browser = await launch({ headless: false });
-  const page = await browser.newPage();
-
-  const site = pickSite('CLIEN');
-  await site.login(page, { loginId: 'jindongp', password: 'lowlow' });
-
-  await sellArticles(page);
 })();
