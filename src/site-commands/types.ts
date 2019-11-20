93f18ee46488bdccc9da179402a90067ca1484c1
JayJayDee <jindongp@gmail.com>
2019-11-20 20:36:07 +0900

    updated Article type.

--- src/site-commands/types.ts
+++ src/site-commands/types.ts
@@ -9,11 +9,21 @@ export type SiteCredential = {
 
 export type SellArticle = {
   url: string;
+  purpose: 'SELL' | 'BUY' | 'EXCHANGE';
   title: string;
+  regDate: Date;
+};
+
+export type SellArticleResponse = {
+  articles: SellArticle[];
+  navigation: {
+    prev: string | null;
+    next: string | null;
+  }
 };
 
 export type Site = {
   site: SiteType;
   login: (page: Page) => Promise<void>;
-  sellArticles: (page: Page) => Promise<SellArticle[]>;
+  sellArticles: (page: Page) => Promise<SellArticleResponse>;
 };
